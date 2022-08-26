f2cc1f09d8c2cb4066950d4152ce468e45dd0832
Santiagocapi <81166067+Santiagocapi@users.noreply.github.com>
2022-08-26 09:45:34 -0300

    Add files via upload

---
+++ ClothingTec/js/script.js
@@ -0,0 +1,57 @@
+let buscar = document.querySelector(".buscar_form");
+
+document.querySelector("#buscar").onclick = () => {
+  buscar.classList.toggle("active");
+  carritoCompras.classList.remove("active");
+  login.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let carritoCompras = document.querySelector(".carrito_compras");
+
+document.querySelector("#carrito").onclick = () => {
+  carritoCompras.classList.toggle("active");
+  buscar.classList.remove("active");
+  login.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let login = document.querySelector(".form_login");
+
+document.querySelector("#login").onclick = () => {
+  login.classList.toggle("active");
+  buscar.classList.remove("active");
+  carritoCompras.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#btn_menu").onclick = () => {
+  navbar.classList.toggle("active");
+  buscar.classList.remove("active");
+  carritoCompras.classList.remove("active");
+  login.classList.remove("active");
+};
+
+/* Carrusel */
+
+var swiper = new Swiper(".carrusel", {
+  loop:true,
+  spaceBetween: 20,
+  autoplay: {
+    delay:7500,
+    disableOnInteraction:false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
